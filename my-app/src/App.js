5d03a67ee0a1488ceb436865a833f2187e94f5c9
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2025-03-14 14:22:01 +0000

    Add loader functional component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import css from "./App.module.css";
 import NavBarForm from "./components/NavBarForm";
 import Sidebar from "./components/Sidebar";
 import Content from "./components/Content";
+import Loader from "./components/Loader";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <NavBarForm />
       <Sidebar />
       <Content />
+      <Loader />
     </div>
   );
 }
