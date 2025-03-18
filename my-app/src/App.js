fea9ed8b4dcb604f568201a9ab0eba20031dc3b5
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2025-03-18 15:37:24 +0000

    Add ContentHooks.js to replace the class compononents on Content.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import NavBarForm from "./components/NavBarForm";
 import Sidebar from "./components/Sidebar";
 import Content from "./components/Content";
 import Loader from "./components/Loader";
+import ContentHooks from "./components/ContentHooks";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/* <NavBarSimple /> */}
       <NavBarForm />
       <Sidebar />
-      <Content />
+      <ContentHooks />
+      {/* <Content /> */}
       <Loader />
     </div>
   );
